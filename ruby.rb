40705958e33919105d9df76e2cb4cbda067b8231
Moshiko Lev <moshiko@jit.io>
2024-01-07 12:05:01 +0200

    Update ruby.rb

--- ruby.rb
+++ ruby.rb
@@ -1,19 +1,40 @@
-class TextPrinter < Object
-    def echo(*args)
-       puts "Arguments: " + args.join(', ')
-    end
+# Example Ruby file with security issues
 
-    def echo_uppercase(*args)
-       puts "Arguments: " + args.join(', ').upcase
-    end
+require 'sinatra'
 
-    def echo_lowercase(*args)</p>
-       puts "Arguments: " + args.join(', ').downcase
-    end
+# Insecure Global Variable
+$global_user_data = {}
+
+get '/set_user' do
+  # User Input Not Sanitized
+  user_name = params['name']
+  user_password = params['password']
+
+  # Storing Sensitive Data Insecurely
+  $global_user_data[user_name] = user_password
+
+  "User #{user_name} added."
 end
 
-method_names = ["echo", "echo_uppercase", "echo_lowercase"]
-tprinter = TextPrinter.new()
-for mn in method_names do
-    tprinter.send(mn, "a", "b", "c", "A", "B", "C")
+get '/get_user' do
+  # Direct Object Reference
+  user_name = params['name']
+
+  # Sensitive Data Exposure
+  "Password for #{user_name} is #{$global_user_data[user_name]}"
 end
+
+# SQL Injection Vulnerability
+get '/unsafe_query' do
+  query = "SELECT * FROM users WHERE name = '#{params['name']}'"
+  # Execute the query directly, without sanitization
+  execute_query(query)
+end
+
+def execute_query(query)
+  # This is a placeholder for query execution
+  # In a real application, this would run the query against the database
+end
+
+# Start the server if this file is run directly
+run! if app_file == $0
