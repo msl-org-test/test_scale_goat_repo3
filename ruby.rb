ac31016b2b9f6f67f1934d8974ca3f72fbd1097b
Moshiko Lev <moshiko@jit.io>
2024-01-30 18:32:35 +0200

    Update ruby.rb

--- ruby.rb
+++ ruby.rb
@@ -7,7 +7,7 @@ $global_user_data = {}
 
 get '/set_user' do
   # User Input Not Sanitized
-  user_name = params['namaa21ddde']
+  user_name = params['namaa21d22dde']
   user_password = params['password']
 
   # Storing Sensitive Data Insecurely
