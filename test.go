fcb714c5ebc802257e5b93710b97cc2d1e7e3e4b
Moshiko Lev <moshiko@jit.io>
2024-03-10 18:16:40 +0200

    Update test.go

--- test.go
+++ test.go
@@ -1,1 +1,1 @@
-listQuery := fmt.Sprintf("SELECT name as '%s', name as '%s', name as '%s' FROM sys.databases", infra.ColumnId, infra.ColumnName, infra.ColumnTagDbName) 
+listQuery := fmt.Sprintf("SELECT name as '%s', name as '%s', name as '%s' FROM sys.databases", infra.ColumnId, infra.ColumnName, infra.ColumnTagDbName) // test
