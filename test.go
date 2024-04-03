5c1e3725f14ef552b0dde312d89fc4f2e75e5607
Moshiko Lev <moshiko@jit.io>
2024-04-03 18:39:51 +0300

    Update test.go

--- test.go
+++ test.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-listQuery := fmt.Sprintf("SELECT name as '%s', name as '%s', name as '%s' FROM sys.databases", infra.ColumnId, infra.ColumnName, infra.ColumnTagDbName) // test
+listQuery := fmt.Sprintf("SELECT name as '%s', name as '%s', name as '%s' FROM sys.dataגגגbases", infra.ColumnId, infra.ColumnName, infra.ColumnTagDbName) // test
 }
