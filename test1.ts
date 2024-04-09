3466b8ebcceedbe230327dd358bd42da2d63a99a
Moshiko Lev <moshiko@jit.io>
2024-04-09 13:39:42 +0300

    Create test1.ts

---
+++ test1.ts
@@ -0,0 +1,6 @@
+export function addLowerCaseEmailWhereIfNeeded({ email }, q: Knex.QueryBuilder) {
+  if (email && q.whereRaw) {
+    // case insensitive email query
+    q.whereRaw(`LOWER("email") = LOWER('${email}')`);
+  }
+}
